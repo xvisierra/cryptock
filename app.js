51933d1873cd32803f36657775de02717a3eac37
Sameer Kumar Singh <xvisierra@gmail.com>
2023-08-04 23:25:54 +0530

    time added

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+function updateDateTime() {
+    const now = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
+    const formattedDateTime = now.toLocaleDateString('en-US', options);
+    document.getElementById('current-date-time').textContent = formattedDateTime;
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000); // Update every second
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
 var btc = document.getElementById("bitcoin");
 var ltc = document.getElementById("litecoin");
 var eth = document.getElementById("ethereum");
